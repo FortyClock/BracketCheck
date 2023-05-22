660246f37b3e9e519e845bb4174b664fb5d6d9e3
FortyClock <parkhom1348@gmail.com>
2023-05-23 00:26:12 +0700

    BracketCheck
    The code сhecks the correctness of the bracket expression

---
+++ BracketCheck.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+void main()
+{
+	// ( { [ <
+
+	string line = ")({}(<>)[[]]";
+	stack <char> container;
+
+	container.push(line[0]);
+
+	for (int i = 1; i < line.length(); i++)
+	{
+		if (line[i] == '(' || line[i] == '[' || line[i] == '{' || line[i] == '<')
+			container.push(line[i]);
+
+		else if (line[i] == ')' ||
+				 line[i] == ']' ||
+				 line[i] == '}' ||
+				 line[i] == '>')
+		{
+			if (container.top() == '(' ||
+				container.top() == '{' ||
+				container.top() == '<' ||
+				container.top() == '[')
+				container.pop();
+			else
+				break;
+		}		
+	}
+
+
+	if (container.empty())
+		cout << true;       // expression is correct
+	else
+		cout << false;		// expression is uncorrect
+
+}
